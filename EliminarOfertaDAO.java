e3d9193864b74d0cd29e2dc7932750792ec5a5ff
MarioHurtadoLopezING <marioolopez21@gmail.com>
2017-04-25 18:55:14 -0500

    Parte de eliminar
    Primera version de la parte de logica de negocios en eliminar oferta

---
+++ EliminarOfertaDAO.java
@@ -0,0 +1,45 @@
+package LogicaNegocio;
+
+import AccesoDatos.ConexionSQL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EliminarOfertaDAO implements InterfazEliminarOfertaSql {
+
+    @Override
+    public void eliminarOferta(int idOferta) {
+        ConexionSQL conexion = new ConexionSQL();
+        Connection conexionInicio=conexion.getConexion();
+        try {    
+            PreparedStatement orden;
+            orden = conexionInicio.prepareStatement("delete from oferta where idOferta = ?");
+            orden.setInt(1, idOferta);
+            orden.execute();
+        } catch (SQLException | NullPointerException excepcion) {
+            Logger logger = Logger.getLogger("Logger");
+            logger.log(Level.WARNING, "La conexión podría ser nula | la sentencia SQL esta mal");
+        }finally{
+            conexion.cerrarConexion();
+        } 
+    }
+    @Override
+    public void eliminarDiasOferta(int idOferta) {
+       ConexionSQL conexion = new ConexionSQL();
+        Connection conexionInicio=conexion.getConexion();
+        try {    
+            PreparedStatement orden;
+            orden = conexionInicio.prepareStatement("delete from Dia where idOferta = ?");
+            orden.setInt(1, idOferta);
+            orden.execute();
+        } catch (SQLException | NullPointerException excepcion) {
+            Logger logger = Logger.getLogger("Logger");
+            logger.log(Level.WARNING, "La conexión podría ser nula | la sentencia SQL esta mal");
+        }finally{
+            conexion.cerrarConexion();
+        }
+    }
+    
+}
---
+++ InterfazEliminarOfertaSql.java
@@ -0,0 +1,6 @@
+package LogicaNegocio;
+
+public interface InterfazEliminarOfertaSql {
+    public void eliminarOferta(int idOferta);
+    public void eliminarDiasOferta(int idOferta);
+}
